9b089136b925f52a0a9112498e42f1546c0e429d
chasyuss <cotn963@gmail.com>
2024-05-20 15:52:19 +0900

    계산기

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
+import { useState } from "react";
+
 export default function App() {
+  const [plus, setPlus] = useState("");
+  const [minus, setMinus] = useState("");
+  const [reult, setResult] = useState("");
+
+  const cal = (e) => {
+    e.preventDefault();
+
+    setMinus("");
+    setPlus("");
+
+  }
+
   return (
     <div>
       <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
       <div>
-        <input /> 만큼을 <button>더할게요</button> <button>뺄게요</button>
+        <input /> 만큼을 <button onChange={(e) => setPlus(e.target.value)}>더할게요</button>
+        <button onChange={(e) => setMinus(e.target.value)}>뺄게요</button>
         <button>초기화</button>
       </div>
       <hr />
       <div>
         <h3>결과</h3>
-        <p>0</p>
+        <p> </p>
       </div>
     </div>
   );
